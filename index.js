a4479a3d3676257ca80d21d65c54cac0159bb332
Matt Johnston <m@ttjohnston.com>
2019-02-21 20:23:06 -0400

    use ncp, no auto cleanup

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const mv = require('mv');
+const cp = require('ncp');
 const homedir = require('os').homedir();
 const pathToServices = !!process.env.pathToServices ||`${homedir}/Library/Services`;
+const filename = 'Open in VSCode.workflow';
 
-mv('./Open in VSCode.workflow', pathToServices, (err) => {
+cp(`./${filename}`, `${pathToServices}/${filename}`, (err) => {
     if(err) {
         console.error(err);
         process.exit(1);
