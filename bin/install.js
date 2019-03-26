7567bc3be7d1ac829f0a0c27c5edc64c9e1cd0df
Matt Johnston <m@ttjohnston.com>
2019-03-26 20:55:12 -0300

    wortkflow name w whitespace, alert for missing bin path

--- bin/install.js
+++ bin/install.js
@@ -3,7 +3,7 @@
 const homedir = require('os').homedir();
 const fse = require('fs-extra');
 const pathToServices = !!process.env.pathToServices ||`${homedir}/Library/Services`;
-const packageName = 'Open-in-VSCode.workflow';
+const packageName = 'Open in VSCode.workflow';
 const dest = `${pathToServices}/${packageName}`;
 
 fse.ensureDirSync(dest);
